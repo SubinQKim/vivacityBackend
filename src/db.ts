e5cf082d7b6513dde3bdd431cb6c026c4065a60f
subinqkim <subinlk@uci.edu>
2023-08-10 23:05:27 -0700

    funInfo obj and insertInfo func

--- src/db.ts
+++ src/db.ts
@@ -8,3 +8,23 @@ const pool = new Pool({
   password: 'nyqcelerity!21Foxglove',
   port: 5432,
 });
+
+type FunInfo = {
+  name: string;
+  hobbies: string[];
+  favoriteFood: string;
+};
+
+async function insertFunInfo(info: FunInfo) {
+  const query = `INSERT INFO fun_info (name, hobbies, favorite_food) VALUES ($1, $2, $3)`;
+  const values = [info.name, info.hobbies, info.favoriteFood];
+
+  try{
+    await pool.query(query, values);
+    console.log('Inserted info into the db')
+  } catch (error) {
+    console.error('Error inserting fun info:', error);
+  }
+}
+
+export {pool, insertFunInfo}
