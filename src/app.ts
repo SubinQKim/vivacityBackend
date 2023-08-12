dca2f68206b7c56f78550892b65fa34b24668508
subinqkim <subinlk@uci.edu>
2023-08-11 23:16:17 -0700

    import of insertFunInfo obj and change from get req to post req for db

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,43 @@
 import express, { Request, Response } from "express";
+import { pool, insertFunInfo } from './db';
 import cors from "cors";
 
 const app = express();
 const port = 3000;
 
-//enable cors
+//enable cors to allow reqs from diff origins
 app.use(cors());
 
-//API endpoint
-app.get("/awesome/applicant", (req: Request, res: Response) =>  {
+// Parse incoming json reqs
+app.use(express.json());
+
+
+app.post('/fun/info', async (req: Request, res: Response) => {
+    const { name, hobbies, favoriteFood } = req.body;
+
     const funInfo = {
-        name: "Subin Kim",
-        hobbies: ["Reading Space Operas", "Redesigning room", "Listening to hip hop"],
-        favoriteCoffee: "Iced Vanilla Latte",
+        name,
+        hobbies,
+        favoriteFood,
     };
-    res.json(funInfo);
+
+    try {
+        await insertFunInfo(funInfo);
+        res.json({ message: 'Fun information inserted successfully' });
+      } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
+      }
 });
+
+//API endpoint (old, before stretch activity)
+// app.get("/awesome/applicant", (req: Request, res: Response) =>  {
+//     const funInfo = {
+//         name: "Subin Kim",
+//         hobbies: ["Reading Space Operas", "Redesigning room", "Listening to hip hop"],
+//         favoriteCoffee: "Iced Vanilla Latte",
+//     };
+//     res.json(funInfo);
+// });
 
 //Start the server
 app.listen(port, () => {
