6bde39fde6390b3c0c47a627c3facd0fc5295dfe
subinqkim <subinlk@uci.edu>
2023-08-14 23:46:17 -0700

    Creating __test__ dir in src dir, also installing supertest package using @types for typescript def. of supertests

---
+++ src/__tests__/app.test.ts
@@ -0,0 +1,17 @@
+import request from 'supertest';
+import app from '../app'; // Import your Express app instance
+
+describe('POST /fun/info', () => {
+  it('should insert fun info into the database', async () => {
+    const response = await request(app)
+      .post('/fun/info')
+      .send({
+        name: 'Test Name',
+        hobbies: ['Hobby 1', 'Hobby 2'],
+        favoriteFood: 'Test Food',
+      });
+
+    expect(response.status).toBe(200);
+    expect(response.body.message).toBe('Fun information inserted successfully');
+  });
+});
