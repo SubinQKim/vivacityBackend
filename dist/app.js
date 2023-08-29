10ae69e1cb736e594a698baf4cec399d231ed053
subinqkim <subinlk@uci.edu>
2023-08-29 15:21:02 -0700

    Changing backend from post to get request, since frontend expects userInfo on pic click. Makes more sense to use get req

--- dist/app.js
+++ dist/app.js
@@ -1,20 +1,10 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.app = void 0;
 const express_1 = __importDefault(require("express"));
-const db_1 = require("./db");
 const cors_1 = __importDefault(require("cors"));
 exports.app = (0, express_1.default)();
 const port = 7001;
@@ -22,22 +12,30 @@ const port = 7001;
 exports.app.use((0, cors_1.default)());
 // Parse incoming json reqs
 exports.app.use(express_1.default.json());
-exports.app.post("/fun/info", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    console.log("Received a POST request to /fun/info");
-    const { name, hobbies, favoriteFood } = req.body;
+// app.post("/fun/info", async (req: Request, res: Response) => {
+//   console.log("Received a POST request to /fun/info");
+//   const { name, hobbies, favoriteFood } = req.body;
+//   const funInfo = {
+//     name,
+//     hobbies,
+//     favoriteFood,
+//   };
+//   try {
+//     await insertFunInfo(funInfo);
+//     res.json({ message: "Fun information inserted successfully" });
+//   } catch (error) {
+//     res.status(500).json({ error: "Internal server error" });
+//   }
+// });
+// API endpoint (old, before stretch activity)
+exports.app.get("/awesome/applicant", (req, res) => {
     const funInfo = {
-        name,
-        hobbies,
-        favoriteFood,
+        name: "Subin Kim",
+        hobbies: ["Reading Space Operas", "Redesigning room", "Listening to hip hop"],
+        favoriteCoffee: "Iced Vanilla Latte",
     };
-    try {
-        yield (0, db_1.insertFunInfo)(funInfo);
-        res.json({ message: "Fun information inserted successfully" });
-    }
-    catch (error) {
-        res.status(500).json({ error: "Internal server error" });
-    }
-}));
+    res.json(funInfo);
+});
 //Start the server
 if (require.main == module) {
     exports.app.listen(port, () => {
--- src/app.ts
+++ src/app.ts
@@ -11,23 +11,33 @@ app.use(cors());
 // Parse incoming json reqs
 app.use(express.json());
 
-app.post("/fun/info", async (req: Request, res: Response) => {
-  console.log("Received a POST request to /fun/info");
+// app.post("/fun/info", async (req: Request, res: Response) => {
+//   console.log("Received a POST request to /fun/info");
 
-  const { name, hobbies, favoriteFood } = req.body;
+//   const { name, hobbies, favoriteFood } = req.body;
 
-  const funInfo = {
-    name,
-    hobbies,
-    favoriteFood,
-  };
+//   const funInfo = {
+//     name,
+//     hobbies,
+//     favoriteFood,
+//   };
 
-  try {
-    await insertFunInfo(funInfo);
-    res.json({ message: "Fun information inserted successfully" });
-  } catch (error) {
-    res.status(500).json({ error: "Internal server error" });
-  }
+//   try {
+//     await insertFunInfo(funInfo);
+//     res.json({ message: "Fun information inserted successfully" });
+//   } catch (error) {
+//     res.status(500).json({ error: "Internal server error" });
+//   }
+// });
+
+// API endpoint (old, before stretch activity)
+app.get("/awesome/applicant", (req: Request, res: Response) =>  {
+    const funInfo = {
+        name: "Subin Kim",
+        hobbies: ["Reading Space Operas", "Redesigning room", "Listening to hip hop"],
+        favoriteCoffee: "Iced Vanilla Latte",
+    };
+    res.json(funInfo);
 });
 
 //Start the server
